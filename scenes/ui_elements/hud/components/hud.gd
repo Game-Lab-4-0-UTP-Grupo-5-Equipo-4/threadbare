1bbd0b144b94ad1a99466862ada70bb2b1526fc5
joacs <gaelmzl49@gmail.com>
2025-06-25 15:43:35 -0500

    Se añadio el apartado inicial

--- scenes/ui_elements/hud/components/hud.gd
+++ scenes/ui_elements/hud/components/hud.gd
@@ -4,6 +4,5 @@ extends CanvasLayer
 
 @onready var story_quest_progress: PanelContainer = %StoryQuestProgress
 
-
 func change_story_quest_progress_visibility(visibility: bool) -> void:
 	story_quest_progress.visible = visibility
